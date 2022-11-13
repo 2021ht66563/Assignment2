cfb4a67e80e3e758d8545893fc515357a9fc025c
2021ht66563 <2021ht66563@wilp.bits-pilani.ac.in>
2022-11-12 22:55:43 -0500

    Revert "1st Change Dev4-AdditionOfNewLogic"
    This reverts commit 0c30f04c53dbdbc79edd9529b0d1a6dd8f3b92ff.

--- CommonFile.py
+++ CommonFile.py
@@ -24,37 +24,3 @@ if flag:
     print(num, "is not a prime number")
 else:
     print(num, "is a prime number")
-
-    num = 407
-
-# To take input from the user
-#num = int(input("Enter a number: "))
-
-# prime numbers are greater than 1
-if num > 1:
-   # check for factors
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"is a prime number")
-       
-# if input number is less than
-# or equal to 1, it is not prime
-else:
-   print(num,"is not a prime number")
-
-
-
-
-   # Python program to check if the input number is odd or even.
-# A number is even if division by 2 gives a remainder of 0.
-# If the remainder is 1, it is an odd number.
-
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
